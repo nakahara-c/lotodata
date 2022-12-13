b6a000b3255140081f062bf480c694903f14089f
nakahara-c <ttcocoan@gmail.com>
2022-12-14 02:17:10 +0900

    add addEventListner

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-const title = document.getElementById('title');
-title.setAttribute('style', 'border:3px solid red');
+window.addEventListener('DOMContentLoaded', () => {
+
+    const title = document.getElementById('title');
+    title.setAttribute('style', 'border:3px solid red');
+
+});
