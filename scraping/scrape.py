19a50301c8a88eda56db6f5ee1ac67211e2de447
nakahara-c <ttcocoan@gmail.com>
2022-12-15 02:51:40 +0900

    separate defs

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -2,22 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+
+def makenewURL(url):
+    num = int(url[-10:-5])
+    num += 20
+    newURL = url[:-10] + str(num) + url[-5:]
+    return newURL
+
+
+def getdata(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    s = soup.select('td')
+    lis = [[''] * 8 for _ in range(20)]
+
+    cnt = 0
+    for i in range(20):
+        for j in range(8):
+            lis[i][j] = s[cnt].get_text()
+            cnt += 1
+    return lis
+
+
+def exportCSV(dataList):
+    exportDirectory = 'C:/test.csv'
+    with open(exportDirectory, 'w', encoding='UTF-8') as f:
+        writer = csv.writer(f)
+        writer.writerows(dataList)
+    return
+
+
 targetURL = 'https://www.mizuhobank.co.jp/retail/takarakuji/check/loto/backnumber/loto60001.html'
-exportDirectory = 'C:/test.csv'
 
-res = requests.get(targetURL)
-soup = BeautifulSoup(res.text, 'html.parser')
-
-s = soup.select('td')
-lis = [[''] * 8 for _ in range(20)]
-
-cnt = 0
-for i in range(20):
-    for j in range(8):
-        lis[i][j] = s[cnt].get_text()
-        cnt += 1
-
-with open(exportDirectory, 'w', encoding='UTF-8') as f:
-    writer = csv.writer(f)
-    writer.writerows(lis)
-
+for _ in range(22):  # 60441まで。60661からは別の方法で…
+    targetURL = makenewURL(targetURL)
+    # currentDataList = getdata(targetURL)
+    # exportCSV(currentDataList)
+    print(targetURL)
