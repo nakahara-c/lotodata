d458537bc9b893c66924540d2b5a7d89b96d74cd
nakahara-c <ttcocoan@gmail.com>
2024-12-02 01:02:14 +0900

    add l6

--- app/page.tsx
+++ app/page.tsx
@@ -16,15 +16,23 @@ export default async function Home() {
   });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">lotodata.net</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <h1 className="text-4xl font-bold mb-4">lotodata.net</h1>
       <table className="table-auto">
         <thead>
           <tr>
+            <th>種類</th>
             <th>抽選日</th>
             <th>回号</th>
-            <th>当せん番号</th>
-            <th>ボーナス数字</th>
+            <th>本数字1</th>
+            <th>本数字2</th>
+            <th>本数字3</th>
+            <th>本数字4</th>
+            <th>本数字5</th>
+            <th>本数字6</th>
+            <th>本数字7</th>
+            <th>ボーナス数字1</th>
+            <th>ボーナス数字2</th>
             <th>口数</th>
             <th>当せん金額</th>
             <th>キャリーオーバー</th>
@@ -32,16 +40,38 @@ export default async function Home() {
         </thead>
         <tbody>
           {results.map((r) => (
-            <tr key={`${r.id}-${r.draw_number}`}>
+            <tr key={`${r.kind}-${r.draw_number}`}
+              className={r.kind === 'L7' ? 'bg-green-100' : 'bg-blue-100'}
+            >
+              <td>{r.kind === 'L7' ? 'ロト7' : 'ロト6'}</td>
               <td>{new Date(r.draw_date).toISOString().slice(0, 10)}</td>
               <td>{r.draw_number}</td>
               <td>
-                {r.number1} {r.number2} {r.number3} {r.number4} {r.number5} {r.number6} {r.number7}
+                {r.number1}
               </td>
               <td>
-                {r.bonus1 && r.bonus2
-                  ? `${r.bonus1}, ${r.bonus2}`
-                  : r.bonus1 || r.bonus2 || 'N/A'}
+                {r.number2}
+              </td>
+              <td>
+                {r.number3}
+              </td>
+              <td>
+                {r.number4}
+              </td>
+              <td>
+                {r.number5}
+              </td>
+              <td>
+                {r.number6}
+              </td>
+              <td>
+                {r.number7 ?? ''}
+              </td>
+              <td>
+                {r.bonus1}
+              </td>
+              <td>
+                {r.bonus2 ?? ''}
               </td>
               <td>{r.winning_tickets || 'N/A'}</td>
               <td>{r.prize_amount || 'N/A'}</td>
